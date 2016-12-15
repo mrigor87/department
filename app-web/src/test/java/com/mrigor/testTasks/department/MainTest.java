76d259966f1a37e9f35c1e886667d364e558e27c
Igor Sych <mrigor87@gmail.com>
2016-12-15 16:38:00 +0300

    web

--- app-web/src/test/java/com/mrigor/testTasks/department/MainTest.java
+++ app-web/src/test/java/com/mrigor/testTasks/department/MainTest.java
@@ -1,19 +1,18 @@
 package com.mrigor.testTasks.department;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 
 /**
  * Created by Igor on 14.12.2016.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class MainTest {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-    @Test
+   // @Test
     public void test1() throws Exception {
         LOG.info("###########################################################################################");
     }
