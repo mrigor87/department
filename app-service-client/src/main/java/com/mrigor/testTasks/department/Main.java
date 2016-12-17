bf310ba1ffa83b9d8acce073ee2e84d7c53fbb36
mrigor87 <mrigor87@gmail.com>
2016-12-17 15:53:48 +0300

    client service OK

--- app-service-client/src/main/java/com/mrigor/testTasks/department/Main.java
+++ app-service-client/src/main/java/com/mrigor/testTasks/department/Main.java
@@ -1,7 +1,17 @@
 package com.mrigor.testTasks.department;
 
+import com.mrigor.testTasks.department.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by Игорь on 17.12.2016.
  */
 public class Main {
+
+
+    public static void main(String[] args) {
+        Main m=new Main();
+     //   m.service.getAll();
+
+    }
 }
--- app-web/src/main/java/com/mrigor/testTasks/department/web/RootController.java
+++ app-web/src/main/java/com/mrigor/testTasks/department/web/RootController.java
@@ -1,10 +1,13 @@
 package com.mrigor.testTasks.department.web;
 
+import com.mrigor.testTasks.department.model.Department;
+import com.mrigor.testTasks.department.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-;
+;import java.util.List;
 
 
 /**
@@ -13,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class RootController {
-
+    @Autowired
+    DepartmentService departmentService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
         System.out.println("!!!!!!!!!!!!@@@@@@@@@@@@@@@!!!!!!!!!!!!!");
+        System.out.println(departmentService.getAll());
+        List<Department> all = departmentService.getAll();
         return "index";
     }
 
